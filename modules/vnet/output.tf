56c7726c0a2bf56d08da485f8bf83fc9b9283ccd
Zaid Barhoun <zaid.barhoun10@gmail.com.com>
2024-10-24 13:40:35 +0100

    Looping using the vms name instead

--- modules/vnet/output.tf
+++ modules/vnet/output.tf
@@ -1,7 +1,7 @@
 
 output "subnet_ids" {
   value = merge(
-    { for vm in var.linux_vms : vm.subnet => azurerm_subnet.test_subnet[vm.subnet].id },
-    { for vm in var.windows_vms : vm.subnet => azurerm_subnet.test_subnet[vm.subnet].id }
+    { for vm in var.linux_vms : vm.name => azurerm_subnet.test_subnet[vm.subnet].id },
+    { for vm in var.windows_vms : vm.name => azurerm_subnet.test_subnet[vm.subnet].id }
   )
 }
